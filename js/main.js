9aef3ebef5d01cddd537089807eb292b64efe91a
harsh-c <harsh_chauhan94@hotmail.com>
2020-09-10 20:34:27 -0700

    Adding work section and scroll reveal in js

--- js/main.js
+++ js/main.js
@@ -25,3 +25,35 @@ function linkAction() {
 }
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// Scroll reveal
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+// Scroll Home
+sr.reveal(".home__title", { reset: false });
+sr.reveal(".button", { delay: 200, reset: false });
+sr.reveal(".home__img", { delay: 400, reset: false });
+sr.reveal(".home__social-icon", { interval: 200, reset: false });
+
+// Scroll About
+// sr.reveal(".about__img", {});
+sr.reveal(".about__subtitle", { delay: 200, reset: false });
+sr.reveal(".about__text", { delay: 400, reset: false });
+
+// Scroll  Skills
+sr.reveal(".skills__subtitle", { reset: false });
+sr.reveal(".skills__text", { delay: 200, reset: false });
+sr.reveal(".skills__data", { interval: 200, reset: false });
+sr.reveal(".skills__img", { interval: 400, reset: false });
+
+// Scroll Work
+// sr.reveal(".work__img", { interval: 200 });
+
+// Scroll contact
+sr.reveal(".contact__input", { interval: 200, reset: false });
